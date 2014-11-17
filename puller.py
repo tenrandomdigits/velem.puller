5e071038e93722a2dba66fbdd11025e957f2a544
tenrandomdigits <tenrandomdigits@gmail.com>
2014-11-17 15:01:50 -0500

    Update puller.py

--- puller.py
+++ puller.py
@@ -1,10 +1,11 @@
 import os
 import glob
 import Tkinter
-import glob2 #need to download and install this.
+import glob2  #need to download and install this.
+import shutil 
 from shutil import copy
-
 from tkFileDialog import askdirectory
+
 top = Tkinter.Tk()
 
  
@@ -33,26 +34,27 @@ fileNameEntry.pack()
 
 
 
-
+#Main Functionality
 def goButtonCallback():
-	os.chdir(rawFolder)
+	os.chdir(rawFolder) #the function will work on this folder
 	fileName = fileNameEntry.get()
-	#list1 = [raw,cop]
-	for f in glob2.glob(*/**/'*' + str(fileName) + '*.*'): #search for files that contain FileName
-		print (f)
-		print (fileName)
-		copy(f,destFolder) 
-		#if f == fileName + '.iiq':
-			#print (fileName)
-		#else: print ('nothing found')
-		
+	fileExt = ['.iiq', '.CR2', '.eip', '.cos'] #need to figure out how to make this not case sensitive
+	settingsDir = (destFolder + '/CaptureOne/Settings70') #create settings folder
+	if not os.path.isdir(settingsDir): #if the settings folder is not there,
+				os.makedirs(settingsDir) #make it
+	for f in glob.glob('*' + str(fileName) + '*' + str(fileExt[1])): #Search for raw file types (right now just .CR2)
+		#print (f)
+		shutil.copy(f,destFolder)
+		print ('Copied RAW - ' + str(f))	
+			
+	for f in glob2.glob('*/**/*' + str(fileName) + '*' + str(fileExt[3])): #search recursive for files that contain FileName .cos
+				shutil.copy(f,settingsDir) # copy the setting files over
+		print ('Copied Settings - ' + str(f))
+					
 		 
 #Go Button
 goButton = Tkinter.Button (top, text = "Go!", command = goButtonCallback)
 goButton.pack()
 
 
-
 top.mainloop()
-
-
